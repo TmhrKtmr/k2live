6029742ad983790fca2ede188311bd49d0301e7c
Tomoihiro Kitamura <evollamin@gmail.com>
2015-01-31 18:38:53 +0900

    タイトル画面で戻るで終了できない不具合修正

---
+++ app/src/main/java/jp/co/music/tnkr/musicgame/TitleActivity.java
@@ -0,0 +1,23 @@
+package jp.co.music.tnkr.musicgame;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+/**
+ * Created by yukumo3621 on 2015/01/31.
+ */
+public class TitleActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_title);
+    }
+
+    public void onStart(View v) {//クリック時に呼ばれる
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+        TitleActivity.this.finish();
+    }
+}
