faeb65926580fcaa6be3beca2c432005e3869710
Tomoihiro Kitamura <evollamin@gmail.com>
2015-02-01 14:45:48 +0900

    Revert "タイトル画面(仮)からの画面遷移(戻るボタンで終了追加)"
    This reverts commit 6a0e31a6d0f33f7c6379184fcd95472ad4c04d4e.

--- app/src/main/java/jp/co/music/tnkr/musicgame/TitleActivity.java
+++ app/src/main/java/jp/co/music/tnkr/musicgame/TitleActivity.java
@@ -16,5 +16,8 @@ public class TitleActivity extends Activity {
         Intent intent = new Intent(this,MainActivity.class);
         startActivity(intent);
         TitleActivity.this.finish();
+//        Intent intent = new Intent();
+//        intent.setClassName("jp.co.music.tnkr.musicgame", "jp.co.music.tnkr.musicgame.MainActivity");
+//        startActivity(intent);
     }
 }
